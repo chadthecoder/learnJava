f03f954bc8152bf78590386d929a64fb6a411631
chadthecoder <mchadamschad@outlook.com>
2020-01-12 08:07:05 -0800

    did more of JAva tutorial
    add Car objects

--- my-app/src/com/chadthecoder/Main.java
+++ my-app/src/com/chadthecoder/Main.java
@@ -9,5 +9,6 @@ public class Main
         System.out.println("Hello World");
 
         Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
+        Car sallyCar = new Car(13.9, "3D20BN", Color.BLACK, false);
     }
 }
