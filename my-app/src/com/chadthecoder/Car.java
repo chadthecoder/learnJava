ba26446196da7d74a55890714a6eabffae42e103
chadthecoder <mchadamschad@outlook.com>
2020-01-11 19:51:04 -0800

    Car class

---
+++ my-app/src/com/chadthecoder/Car.java
@@ -0,0 +1,20 @@
+package com.chadthecoder;
+import java.awt.*;
+
+public class Car {
+    double averageMilesPerGallon;
+    String licensePlate;
+    Color paintColor;
+    boolean areTailLightsWorking;
+
+    public Car(double inputAverageMPG,
+               String inputLicensePlate,
+               Color inputPaintColor,
+               boolean inputAreTailLightsWorking)
+    {
+        this.averageMilesPerGallon = inputAverageMPG;
+        this.licensePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.areTailLightsWorking = inputAreTailLightsWorking;
+    }
+}
---
+++ my-app/src/com/chadthecoder/Main.java
@@ -0,0 +1,13 @@
+package com.chadthecoder;
+import java.awt.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	    // write your code here
+        System.out.println("Hello World");
+
+        Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
+    }
+}
