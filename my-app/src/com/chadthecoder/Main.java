5965edb820cdf1c3e95ab191b0cb81aa4765ba89
chadthecoder <mchadamschad@outlook.com>
2020-01-14 22:45:36 -0800

    my-app tutorial, printing license plates

--- my-app/src/com/chadthecoder/Main.java
+++ my-app/src/com/chadthecoder/Main.java
@@ -10,5 +10,8 @@ public class Main
 
         Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
         Car sallyCar = new Car(13.9, "3D20BN", Color.BLACK, false);
+
+        System.out.println("My Car's License Plate: " + myCar.licensePlate);
+        System.out.println("Sally's License Plate: " + sallyCar.licensePlate);
     }
 }
